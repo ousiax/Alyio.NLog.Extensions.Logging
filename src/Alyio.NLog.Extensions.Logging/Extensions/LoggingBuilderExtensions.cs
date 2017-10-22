ff72ef61dacd3b02c3f8bfaf85fb8e9689dcbdaa
qqbuby <qqbuby@gmail.com>
2017-10-22 15:21:50 +0800

    Add LoggingBuilderExtensions.cs

---
+++ src/Alyio.NLog.Extensions.Logging/Extensions/LoggingBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Alyio.NLog.Extensions.Logging
+{
+    /// <summary>
+    /// Extension methods for setting up logging service in an <see cref="ILoggingBuilder"/>
+    /// </summary>
+    public static class LoggingBuilderExtensions
+    {
+        /// <summary>
+        /// Add a NLog provider.
+        /// </summary>
+        /// <param name="builder"><see cref="ILoggingBuilder"/>.</param>
+        /// <returns><see cref="ILoggingBuilder"/></returns>
+        public static ILoggingBuilder AddNLog(this ILoggingBuilder builder)
+        {
+            builder.AddProvider(new NLoggerProvider());
+            return builder;
+        }
+    }
+}
